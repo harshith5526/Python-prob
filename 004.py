fe859b77d1ed6aeaba1dafa5f164de1c1f45dfcf
Harshith A K <harshithak5520@gmail.com>
2026-02-15 11:33:14 +0530

    Create 004.py
    program to perform the use of continue and pass statement

---
+++ 004.py
@@ -0,0 +1,21 @@
+# program to perform the use of continue statement
+
+# print the odd numbers from 1 to 10 using continue statement
+for i in range(1,10):
+  if (i%2==0):
+    continue
+  print(i)
+
+
+# using while loop
+num=0
+while num<10:
+  num=num+1
+  if(num%2==0):
+    continue
+  print(num)
+
+# program to show the use of pass statement
+#inside a function
+def future_function:
+pass   # placeholder for code to be written later
