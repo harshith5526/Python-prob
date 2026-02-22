37b53caf662c8dbce7f6a9d87907a4e5200a28d0
Harshith A K <harshithak5520@gmail.com>
2026-02-22 20:26:41 +0530

    Implement list rotation based on user-defined position
    Added functionality to rotate a list to the right based on user input.

--- 005.py
+++ 005.py
@@ -1,1 +1,22 @@
+# python script to rotate right about a given position in that list and display them.
 
+# Create a List
+myList =[1,4,5,-10]
+
+print("List before rotation = ",myList)
+
+# The value of n for rotation position
+
+n=int(input("Enter rotation position:"))
+
+# Rotating the List
+
+if n>len(myList):
+
+n = int(n%len(myList))
+
+myList = (myList[-n:] + myList[:-n])
+
+# Display the Update List after rotation
+
+print("Updated List after rotation = ",myList)
